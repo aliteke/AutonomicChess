eb57b928dafd4ab769485d8561df90b04b8e87dd
Andrew Bengtson <agbengtson@gmail.com>
2024-11-22 23:04:50 -0500

    updates-11-22-pt-2

---
+++ README.txt
@@ -0,0 +1,7 @@
+in order to run this program first use pip to install the packages chess, pandas, and matplotlib
+A demonstration testing the autonomic manager in action can be run with the following command
+py .\runAutonomicChessTest.py
+
+for bayesian bot detector make sure to additionally install pgmpy and daft
+For a very simple demonstration of the technical basis for the proposed bayesian bot detector run the following command:
+python .\testBayes.py
--- runAutonomicChessTest.py
+++ runAutonomicChessTest.py
@@ -4,26 +4,27 @@ import chess.engine
 import chess.svg
 from autonomicChessManager import autonomicChessManager
 import random
-import time
 import matplotlib.pyplot as plt
-import numpy as np
+import pandas as pd
 #these params are used to setup the test (can also be though of as high level policy for the autonomic system)
-numIters = 50
+numIters = 1000
 moveTimeoutDuration = 0.1
 
 #this script will be used in order to run tests on the autonomic chess system
 #hardcode the "optimal" engine as well as the test engine's paths
 dirname = os.path.dirname(__file__)
-testEngineDirPath = os.path.join(dirname, 'chessEngineTestDirectory')
-#TODO stockfish used as an optimal engine right now for testing because it is radically more reliable than others
+testEngineDirPath = os.path.join(dirname, 'chessEngineTestReformed')
 optimalEnginePath = os.path.join(dirname, 'optimalEngineDirectory\stockfish-windows-x86-64-avx2.exe')
+#store the optimal opponent mapping, using the CCRL competition feature this will be the opponent each bot is weakest against
+optimalMatch = { "stockfish-windows-x86-64-avx2.exe" : "stockfish-windows-x86-64-avx2.exe", "Clover.6.1-avx2.exe" : "igel-x64_popcnt_avx2_3_5_0.exe", "Alexandria-6.1.0-avx2.exe" : "stockfish-windows-x86-64-avx2.exe", "igel-x64_popcnt_avx2_3_5_0.exe": "stockfish-windows-x86-64-avx2.exe", "Uralochka3.40a-avx2.exe": "Uralochka3.40a-avx2.exe" }
 
 #for loop in which monte-carlos will be run
 for iter in range(0,numIters):
+    df = pd.DataFrame(columns=['turn','move_made','player','winner','detected?'])
     #initialize an empty board
-    board = chess.Board()
+    board = chess.Board()    
     #initialiaze the autonomic manager
-    manager = autonomicChessManager(optimalEnginePath,testEngineDirPath,moveTimeoutDuration)
+    manager = autonomicChessManager(optimalEnginePath,testEngineDirPath,moveTimeoutDuration,optimalMatch)
 
     #draw an opponent from the test set
     opponentName = random.choice(os.listdir(testEngineDirPath))
@@ -39,11 +40,13 @@ for iter in range(0,numIters):
             result = opponentEngine.play(board, chess.engine.Limit(time=moveTimeoutDuration))
             board.push(result.move)
             print("opponent played",result.move)
+            df = df._append({'turn' : len(board.move_stack),'move_made': result.move.uci(),'player':'opponent,'+opponentName,"detected?":manager.engineDetected},ignore_index = True)
         #else let manager play
         else:
             #use the makeNextMove command
             moveToMake = manager.makeNextMove(board)
             board.push(moveToMake)
+            df = df._append({'turn' : len(board.move_stack),'move_made': moveToMake.uci(),'player':'autonomic',"detected?":manager.engineDetected},ignore_index = True)
         #log anything we'd want to keep on a turn-by-turn basis
         #boardsvg = chess.svg.board(board, size=350)
 
@@ -61,8 +64,9 @@ for iter in range(0,numIters):
         plt.plot(manager.plot_x,y_vals,label=manager.defeatorsList[count].botName)
         plt.xlabel("number of moves")
         plt.ylabel("similarity score")
-        plt.title("Similarity over time for game played against " + opponentName)
-        plt.figlegend()
+        plt.title("Game played against " + opponentName + " bot detected " + manager.engineDetected)
+        #plt.figlegend()
         plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
         plt.tight_layout()
         plt.savefig('detectorTest/outputMapping'+opponentName+str(iter)+".png")
+    df.to_csv("GameAgainst"+opponentName+str(iter)+".csv")
